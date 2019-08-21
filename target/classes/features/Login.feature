7772178961f10566efffe58d7e91c7ecd264d68f
abha <abha@example.com>
2019-08-21 17:51:51 +0530

    How_to_Run_Cucumber_Test_as_Maven_Test. Now I am able to execute Cucumber test cases as unit test during build process

--- target/classes/features/Login.feature
+++
@@ -1,8 +0,0 @@
-Feature: FreeCRM login feature
-Scenario: Validation of successful login
-
-Given user is already on the Login Page
-When title of the login page is correct
-Then user enters the login credential
-Then user clicks on Login button
-And user is on Home Page
--- test-output/report.js
+++ test-output/report.js
@@ -43,35 +43,35 @@ formatter.match({
   "location": "LoginValidation.user_is_already_on_the_Login_Page()"
 });
 formatter.result({
-  "duration": 38029031534,
+  "duration": 68102456100,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginValidation.title_of_the_login_page_is_correct()"
 });
 formatter.result({
-  "duration": 39382024,
+  "duration": 201985700,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginValidation.user_enters_the_login_credential()"
 });
 formatter.result({
-  "duration": 9219944065,
+  "duration": 14178255600,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginValidation.user_clicks_on_Login_button()"
 });
 formatter.result({
-  "duration": 33356,
+  "duration": 36900,
   "status": "passed"
 });
 formatter.match({
   "location": "LoginValidation.user_is_on_Home_Page()"
 });
 formatter.result({
-  "duration": 31432,
+  "duration": 26500,
   "status": "passed"
 });
 });
